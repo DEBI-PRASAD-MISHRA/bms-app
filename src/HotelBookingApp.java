7c14e6d9f8defa96bb0a05eb9c582ab2358ec57d
DEBI-PRASAD-MISHRA <joojooofpuri@gmail.com>
2026-04-06 20:30:59 +0530

    UC2: Hardcode basic room types and availability

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -9,5 +9,14 @@ public class HotelBookingApp {
         System.out.println("1. View Room Options");
         System.out.println("2. Exit");
         System.out.println("Please select an option:");
+        
+        showRoomOptions();
+    }
+
+    private static void showRoomOptions() {
+        System.out.println("\n--- Room Types & Availability ---");
+        System.out.println("Single - Available");
+        System.out.println("Double - Not Available");
+        System.out.println("Suite  - Available");
     }
 }
