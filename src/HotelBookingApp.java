6c29b9e76869d5f727f1c4e12b172afef79422b4
DEBI-PRASAD-MISHRA <joojooofpuri@gmail.com>
2026-04-06 20:29:39 +0530

    UC1: Application Entry & Welcome Message

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -3,6 +3,11 @@ import java.io.*;
 
 public class HotelBookingApp {
     public static void main(String[] args) {
-        
+        System.out.println("=========================================");
+        System.out.println("   Welcome to Hotel Booking System");
+        System.out.println("=========================================");
+        System.out.println("1. View Room Options");
+        System.out.println("2. Exit");
+        System.out.println("Please select an option:");
     }
 }
